6576917b6a1831bbe84bd1d43e430be9ead01cad
B11rdman <arsenmazmanyan11@gmail.com>
2023-12-18 00:20:09 +0400

    Added new images.

--- src/assets/assetsNames/atlas.ts
+++ src/assets/assetsNames/atlas.ts
@@ -1,4 +1,5 @@
 export const atlases: { name: string; json: string; png: string }[] = [
     { name: 'game-ui', json: 'assets/atlas/game-ui@1.png.json', png: 'assets/atlas/game-ui.png' },
     { name: 'items', json: 'assets/atlas/items@1.png.json', png: 'assets/atlas/items.png' },
+    { name: 'ui', json: 'assets/atlas/ui@1.png.json', png: 'assets/atlas/ui.png' },
 ];

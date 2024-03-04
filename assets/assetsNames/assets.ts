0428f076d9313fe16cf73e6c8445ef7477efb4d0
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2024-03-04 18:30:43 +0000

    Deploying to gh-pages from @ Birdman1104/slots-demo@5dac51538167305e48c5d091a18db345f17ff5e3 🚀

---
+++ assets/assetsNames/assets.ts
@@ -0,0 +1,3 @@
+export const assets: AssetNameAndPath[] = [
+    { name: 'main_background.jpg', path: 'assets/uncompressed/main_background.jpg' },
+];
---
+++ assets/assetsNames/fonts.ts
@@ -0,0 +1,4 @@
+export const fonts: AssetNameAndPath[] = [
+    { name: 'Filmotype_Major', path: 'assets/fonts/Filmotype_Major.otf' },
+    { name: 'SaranaiGame', path: 'assets/fonts/SaranaiGame.ttf' },
+];
